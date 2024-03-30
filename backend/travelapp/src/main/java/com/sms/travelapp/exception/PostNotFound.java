0a785fc3cea109712fe363a7cb2a691e5ccf2e51
Michal Tomaszewski <groobytt@gmail.com>
2024-03-30 11:27:06 +0100

    ✨ add Post Not Found Exception

---
+++ backend/travelapp/src/main/java/com/sms/travelapp/exception/PostNotFound.java
@@ -0,0 +1,12 @@
+package com.sms.travelapp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class PostNotFound extends RuntimeException{
+
+    public PostNotFound(String message){
+        super(message);
+    }
+}
