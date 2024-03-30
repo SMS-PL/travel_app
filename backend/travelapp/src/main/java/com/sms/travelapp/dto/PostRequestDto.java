72115ac521c8d58af41e3a0e278a7aa061e2ed4a
Michal Tomaszewski <groobytt@gmail.com>
2024-03-30 11:30:15 +0100

    🔨 Add validators

--- backend/travelapp/src/main/java/com/sms/travelapp/dto/PostRequestDto.java
+++ backend/travelapp/src/main/java/com/sms/travelapp/dto/PostRequestDto.java
@@ -1,13 +1,20 @@
 package com.sms.travelapp.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.sql.Timestamp;
 
 @Data
 public class PostRequestDto {
+
+    @NotBlank(message = "Content cannot be empty")
     private String content;
+
+    @NotNull(message = "Country id cannot be empty")
     private Long countryId;
+
+    @NotBlank(message = "ImageUrl cannot be empty")
     private String imageUrl;
-    private Long authorId;
 }
