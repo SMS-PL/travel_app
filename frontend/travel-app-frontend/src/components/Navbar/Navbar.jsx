8e3806373ee7fdaead857d312e481df83b00bc42
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-04-11 23:01:38 +0200

    🔨 rebuild navbar, add authorization check

--- frontend/travel-app-frontend/src/components/Navbar/Navbar.jsx
+++ frontend/travel-app-frontend/src/components/Navbar/Navbar.jsx
@@ -1,49 +1,54 @@
 import { Link } from "react-router-dom";
-
-// import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils";
-// import { CommandMenu } from "@/components/command-menu"
 import { Icons } from "@/components/icons";
 import { MainNav } from "./main-nav";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { UserNav } from "./user-nav";
-
-// import { buttonVariants } from "@/registry/new-york/ui/button"
+import useIsAuthenticated from 'react-auth-kit/hooks/useIsAuthenticated';
 
 export function Navbar() {
-    
+    const isAuthenticated = useIsAuthenticated();
 
     return (
-        <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="container flex h-14 max-w-screen-2xl items-center">
-                <MainNav />
-                <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-                    <div className="w-full flex-1 md:w-auto md:flex-none">
-                        {/* <CommandMenu /> */}
-                    </div>
-                    <nav className="flex items-center">
-                        <Link href="#" target="_blank" rel="noreferrer">
-                            <div>
-                                <Icons.gitHub className="h-4 w-4" />
-                                <span className="sr-only">GitHub</span>
-                            </div>
-                        </Link>
-                        <Link
-                            //   href={siteConfig.links.twitter}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <div>
-                                <Icons.twitter className="h-3 w-3 fill-current" />
-                                <span className="sr-only">Twitter</span>
-                            </div>
-                        </Link>
-                        <ModeToggle />
-                        <UserNav />
-                    </nav>
+        <nav className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+            <div className="container grid grid-cols-3 h-14 max-w-screen-2xl items-center">
+                
+                {/* 1 kolumna */}
+                <div className="mr-4 col-span-1 flex">
+                    <Link to="/" className="mr-6 flex items-center space-x-2">
+                        <Icons.logo className="h-6 w-6" />
+                        <span className="font-bold inline-block">
+                            Travel App
+                        </span>
+                    </Link>
                 </div>
+
+                {/* 2 kolumna */}
+                <div className="col-span-1 flex justify-center">
+                    <div className="flex flex-row w-[200px] justify-around">
+                    { isAuthenticated() ?
+                        <>
+                            <Link to="/">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="fill-foreground h-8"><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
+                            </Link>
+                            <Link to="/friends-feed">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" className="fill-foreground h-8"><path d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM609.3 512H471.4c5.4-9.4 8.6-20.3 8.6-32v-8c0-60.7-27.1-115.2-69.8-151.8c2.4-.1 4.7-.2 7.1-.2h61.4C567.8 320 640 392.2 640 481.3c0 17-13.8 30.7-30.7 30.7zM432 256c-31 0-59-12.6-79.3-32.9C372.4 196.5 384 163.6 384 128c0-26.8-6.6-52.1-18.3-74.3C384.3 40.1 407.2 32 432 32c61.9 0 112 50.1 112 112s-50.1 112-112 112z"/></svg>
+                            </Link>
+                            <Link to="/location-feed">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="fill-foreground h-8"><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm306.7 69.1L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>
+                            </Link> 
+                        </> : null }
+                    </div>
+                </div>
+
+                {/* 3 kolumna */}
+                <div className="col-span-1 flex justify-end">
+                    <ModeToggle className="mx-3"/>
+                    { isAuthenticated() ? <UserNav  /> : null }
+                </div>
+
             </div>
-        </header>
+        </nav>
     );
 }
 
