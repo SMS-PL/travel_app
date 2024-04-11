3a2d96ff6bb19c28eeeb3fa40a657b67b6910dbc
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-04-11 23:00:23 +0200

    ✨ add main container

---
+++ frontend/travel-app-frontend/src/components/MainContainer/MainContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
+
+function MainContainer({children}) {
+    return (
+        <div className="flex flex-col items-start min-h-screen">
+            <Navbar />
+            <main className="container relative mx-auto flex flex-col items-center justify-start py-10">
+                {children}
+            </main>
+            <Footer />
+        </div>
+        
+            
+        
+    )
+}
+
+export default MainContainer;
