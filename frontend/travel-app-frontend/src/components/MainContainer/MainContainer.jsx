120db2665be9e673e87ef287e9748b1e0d0f30d8
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-04-17 01:22:09 +0200

    🔨 rebuild main cotainer

--- frontend/travel-app-frontend/src/components/MainContainer/MainContainer.jsx
+++ frontend/travel-app-frontend/src/components/MainContainer/MainContainer.jsx
@@ -1,19 +1,62 @@
 import React from 'react'
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
+import AddPost from "@/components/AddPost/AddPost";
+import Pins from "@/components/Pins/Pins";
 
-function MainContainer({children}) {
+function MainContainer({children, type = "default"}) {
     return (
         <div className="flex flex-col items-start min-h-screen">
             <Navbar />
-            <main className="container relative mx-auto flex flex-col items-center justify-start py-5 ">
-                {children}
+
+            <main className="w-full flex flex-col justify-center items-center">
+                {type == "homeFeed" ? (
+                    <div className="max-w-full w-[700px]">
+                        <h2 className="text-center scroll-m-20 text-lg font-extrabold tracking-tight lg:text-4xl py-5">
+                            Home
+                        </h2>
+                        <Pins />
+                        <AddPost />
+                        {children}
+                    </div>
+                    
+                ) : null}
+
+                {type == "friendsFeed" ? (
+                    <div className="max-w-full w-[700px]">
+                        <h2 className="text-center scroll-m-20 text-lg font-extrabold tracking-tight lg:text-4xl py-5">
+                            Friends
+                        </h2>
+                        <Pins />
+                        <AddPost />
+                        {children}
+                    </div>
+                    
+                ) : null}
+
+                {type == "exploreFeed" ? (
+                    <div className="max-w-full w-[700px]">
+                        <h2 className="text-center scroll-m-20 text-lg font-extrabold tracking-tight lg:text-4xl py-5">
+                            Explore
+                        </h2>
+                        <Pins />
+                        <AddPost />
+                        {children}
+                    </div>
+                    
+                ) : null}
+
+                {type == "default" ? (
+                    <div className="max-w-full w-[700px] flex justify-center items-center">
+                        {children}
+                    </div>
+
+
+                ) : null}
             </main>
+
             <Footer />
         </div>
-        
-            
-        
     )
 }
 
