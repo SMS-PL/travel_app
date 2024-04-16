db407aefd75cde362ca2f40c3213a59da1198161
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-04-17 01:21:30 +0200

    🔨 rebuild home page

--- frontend/travel-app-frontend/src/pages/Home/Home.jsx
+++ frontend/travel-app-frontend/src/pages/Home/Home.jsx
@@ -47,64 +47,21 @@ const Home = () => {
 
 
     return (
-        <MainContainer >
-            <div className="max-w-full w-[650px]">
-
-                <AddPost />
-
-                {posts.map((post) => 
-                    (
-                        <Post
-                            className="mt-10"
-                            key={post.id}
-                            id={post.id}
-                            content={post.content}
-                            countryId={post.countryId}
-                            imageUrl={post.imageUrl}
-                            authorId={post.authorId}
-                            createdAt={post.createdAt}
-                            lastUpdated={post.lastUpdated}
-                        />
-                    )
-                )}
-
-				{/* <Pagination>
-					<PaginationContent>
-						<PaginationItem>
-							<PaginationPrevious href="#" />
-						</PaginationItem>
-
-						<PaginationItem>
-							<PaginationLink href="#">1</PaginationLink>
-						</PaginationItem>
-
-						<PaginationItem>
-							<PaginationLink href="#">2</PaginationLink>
-						</PaginationItem>
-
-						<PaginationItem>
-							<PaginationLink href="#">3</PaginationLink>
-						</PaginationItem>
-
-						<PaginationItem>
-							<PaginationEllipsis />
-						</PaginationItem>
-
-						<PaginationItem>
-							<PaginationNext href="#" />
-						</PaginationItem>
-
-					</PaginationContent>
-				</Pagination> */}
-                
-            </div>
+        <MainContainer type="homeFeed">
+			{posts.map((post) => (
+				<Post
+					key={post.id}
+					id={post.id}
+					content={post.content}
+					countryId={post.countryId}
+					imageUrl={post.imageUrl}
+					authorId={post.authorId}
+					createdAt={post.createdAt}
+					lastUpdated={post.lastUpdated}
+				/>
+			))}
         </MainContainer>
     )
 }
 
-export default Home
-
-{/* <div className="p-4">
-    <button type='submit' className='p-2 border rounded-md bg-cyan-700 hover:bg-cyan-900 text-white' onClick={()=> navigate('/login')}>Go to Login</button>
-    <button type='submit' className='p-2 border rounded-md bg-red-500 hover:bg-red-700 text-white' onClick={()=> navigate('/secure')}>GO to Secure Dashboard</button>
-</div> */}
+export default Home;
