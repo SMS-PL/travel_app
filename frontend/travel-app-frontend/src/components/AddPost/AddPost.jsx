3a520928821a50592e8316fd3287e7073c78dd74
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-04-17 01:18:39 +0200

    🎨 change view of adding post sections

--- frontend/travel-app-frontend/src/components/AddPost/AddPost.jsx
+++ frontend/travel-app-frontend/src/components/AddPost/AddPost.jsx
@@ -19,13 +19,14 @@ import { useNavigate } from 'react-router-dom';
 import { Textarea } from "@/components/ui/textarea";
 import { useForm } from "react-hook-form";
 import { useToast } from "@/components/ui/use-toast";
-import useAuthUser from "react-auth-kit/hooks/useAuthUser";
+
+import { Icons } from "@/components/icons";
+ 
 
 function AddPost() {
 	const authHeader = useAuthHeader();
 	const navigate = useNavigate();
     const { toast } = useToast();
-	const auth = useAuthUser();
 
     const {
         register,
@@ -56,7 +57,7 @@ function AddPost() {
                 return response.json();
             })
             .then(data => {
-                window.location.reload();  // odświeża stronę, aby zobaczyć nowy post -> rozwiązanie tymczasowe
+                //window.location.reload();  // odświeża stronę, aby zobaczyć nowy post -> rozwiązanie tymczasowe
                 toast({
                     title: "Hurrah!",
                     description: "Post added correctly!",
@@ -80,55 +81,34 @@ function AddPost() {
     };
 
     return (
-        <Card>
+        <Card className="mt-5 w-full">
             <form onSubmit={handleSubmit(onSubmit)} className="">
-                <CardHeader className="flex flex-col items-center">
-                    <div >
-                        <h1 className="text-center  scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-4xl">
-                            Hi <span className="text-primary">{auth.firstName}</span>!
-                        </h1>
-                        <h3 className="pb-5 text-center font-semibold scroll-m-20 text-xl tracking-tight py-2">
-                            Tell us where are you?
-                        </h3>
-                    </div>
-
-                    <div className="flex flex-row items-center w-full">
-                        <Avatar>
-                            <AvatarImage src="https://picsum.photos/200/200" alt="stock img" />
-                            <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
-                        
-                        <div className="w-full ml-4">
-                            <Textarea
-                                id="description"
-                                placeholder="Tell us a little bit about yourself"
-                                className="resize-none"
-                                {...register("description", {
-                                    required: "Description is required",
-                                })}
-
-                            />
-                            <p className="text-red-600">{errors.content}</p>
-                        </div>
-                    </div>
-                </CardHeader>
+                {/* <CardHeader className="flex flex-col p-3"></CardHeader> */}
                 
-                {/* <CardContent> </CardContent> */}
-                <CardFooter className="flex justify-between">
+                <CardContent className="flex flex-row items-center w-full pt-5"> 
+                    <Avatar>
+                        <AvatarImage src="https://picsum.photos/200/200" alt="stock img" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
                     
-                        {/* <Input
-                            id="img"
-                            type="file"
-                            // {...register("email", {
-                            //     required: "Email is required",
-                            //     pattern: {
-                            //         value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i,
-                            //         message: "Email is not validated",
-                            //     },
-                            // })}
-                        /> */}
+                    <div className="w-full ml-4">
+                        <Textarea
+                            id="description"
+                            placeholder="Tell us a little bit about yourself"
+                            className="resize-y;"
+                            {...register("description", {
+                                required: "Description is required",
+                            })}
 
-                    <Button variant="outline" type="submit" className="bg-primary w-full">Add post</Button>
+                        />
+                        <p className="text-red-600">{errors.content}</p>
+                    </div>
+                </CardContent>
+
+
+                <CardFooter className="flex justify-between gap-5">
+                    <Button variant="secondary" className="w-full text-foreground"><Icons.imageAdd className="h-6 w-6 fill-foreground mr-1"/> Add photo</Button>
+                    <Button variant="default" type="submit" className="bg-primary w-full text-foreground"><Icons.send className="h-6 w-6 fill-foreground mr-1"/>Add post</Button>
                 </CardFooter>
             </form>
         </Card>
