086420a834bc7d150b3fbf79d2f569192a50ecb5
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-04-16 18:14:26 +0200

    ✨add support for fetching posts data for home feed

--- frontend/travel-app-frontend/src/pages/Home/Home.jsx
+++ frontend/travel-app-frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,102 @@
 import { useNavigate } from 'react-router-dom';
-import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
-import Navbar from "../../components/Navbar/Navbar";
+import useAuthHeader from 'react-auth-kit/hooks/useAuthHeader';
 import MainContainer from "@/components/MainContainer/MainContainer";
+import { useState, useEffect } from 'react';
 
+import Post from "@/components/Post/Post";
+import AddPost from "@/components/AddPost/AddPost";
+
+import {
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from "@/components/ui/pagination"
+  
 const Home = () => {
+	const [posts, setPosts] = useState([]);
+	const authHeader = useAuthHeader();
+	const navigate = useNavigate();
+
+    useEffect(() => {
+		fetch("http://localhost:5000/api/v1/posts/feed?feedType=home&pageSize=10&pageNumber=0", {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json', 
+				"Authorization": authHeader,
+			},
+		})
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Błąd sieci!');
+			}
+			return response.json();
+		})
+		.then(data => {
+			setPosts(data.content);
+		})
+		.catch(error => {
+			console.log(error.message);
+			navigate("/");
+		});
+
+	}, []);
+
 
     return (
         <MainContainer >
-            <h3 className="font-black text-2xl">HOME</h3>
+            <div className="">
+                <AddPost />
+
+                {posts.map((post) => 
+                    (
+                        <Post
+                            className="mt-10"
+                            key={post.id}
+                            id={post.id}
+                            content={post.content}
+                            countryId={post.countryId}
+                            imageUrl={post.imageUrl}
+                            authorId={post.authorId}
+                            createdAt={post.createdAt}
+                            lastUpdated={post.lastUpdated}
+                        />
+                    )
+                )}
+
+				{/* <Pagination>
+					<PaginationContent>
+						<PaginationItem>
+							<PaginationPrevious href="#" />
+						</PaginationItem>
+
+						<PaginationItem>
+							<PaginationLink href="#">1</PaginationLink>
+						</PaginationItem>
+
+						<PaginationItem>
+							<PaginationLink href="#">2</PaginationLink>
+						</PaginationItem>
+
+						<PaginationItem>
+							<PaginationLink href="#">3</PaginationLink>
+						</PaginationItem>
+
+						<PaginationItem>
+							<PaginationEllipsis />
+						</PaginationItem>
+
+						<PaginationItem>
+							<PaginationNext href="#" />
+						</PaginationItem>
+
+					</PaginationContent>
+				</Pagination> */}
+                
+            </div>
         </MainContainer>
     )
 }
