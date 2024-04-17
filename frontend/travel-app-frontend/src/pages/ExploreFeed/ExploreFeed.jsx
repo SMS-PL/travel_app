89d410edd6ab178b56157103d2f3293bed78730e
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-04-18 00:00:30 +0200

    🎨 add temporary view of explore feed

--- frontend/travel-app-frontend/src/pages/ExploreFeed/ExploreFeed.jsx
+++ frontend/travel-app-frontend/src/pages/ExploreFeed/ExploreFeed.jsx
@@ -28,79 +28,32 @@ import {
 import { Skeleton } from "@/components/ui/skeleton";
 
 const FriendsFeed = () => {
-	const [posts, setPosts] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
 	const authHeader = useAuthHeader();
-	const navigate = useNavigate();
-
-    useEffect(() => {
-		setIsLoading(true);
-		// fetch("http://localhost:5000/api/v1/posts/feed?feedType=friends&pageSize=10&pageNumber=0", {
-		// 	method: 'GET',
-		// 	headers: {
-		// 		'Content-Type': 'application/json', 
-		// 		"Authorization": authHeader,
-		// 	},
-		// })
-		// .then(response => {
-		// 	if (!response.ok) {
-		// 		throw new Error('Błąd sieci!');
-		// 	}
-		// 	return response.json();
-		// })
-		// .then(data => {
-		// 	setPosts(data.content);
-		// 	setIsLoading(false);
-		// })
-		// .catch(error => {
-		// 	console.log(error.message);
-		// 	navigate("/");
-		// });
-
-	}, []);
 
 
     return (
         <MainContainer type="exploreFeed">
+			<Card className="mt-5 w-full">
+				<CardHeader className="flex flex-row">
+					<Skeleton className="h-12 w-12 rounded-full" />
 
-			{isLoading ? (
-				<Card className="mt-5 w-full">
-					<CardHeader className="flex flex-row">
-						<Skeleton className="h-12 w-12 rounded-full" />
+					<div className="px-2 w-fit">
+						<Skeleton className="h-4 w-[250px]" />
+						<Skeleton className="h-4 w-[200px] mt-2" />
+					</div>
 
-						<div className="px-2 w-fit">
-							<CardTitle><Skeleton className="h-4 w-[250px]" /></CardTitle>
-							<CardDescription className="text-nowrap"><Skeleton className="h-4 w-[250px] mt-2" /></CardDescription>
-						</div>
+				</CardHeader>
 
-					</CardHeader>
-
-					<CardContent>
-						<Skeleton className="h-14 w-full mb-5" />
-						<Skeleton className="w-full h-[500px]" />
-					</CardContent>
-					
-					<CardFooter className="flex justify-between">
-						<Button variant="outline">Likes</Button>
-						<Button variant="outline">Comments</Button>
-					</CardFooter>
-				</Card>
-
-			) : (
-				posts.map((post) => (
-					<Post
-						key={post.id}
-						id={post.id}
-						content={post.content}
-						countryId={post.countryId}
-						imageUrl={post.imageUrl}
-						authorId={post.authorId}
-						createdAt={post.createdAt}
-						lastUpdated={post.lastUpdated}
-					/>
-				))
-			)}
-
+				<CardContent>
+					<Skeleton className="h-14 w-full mb-5" />
+					<Skeleton className="w-full h-[500px]" />
+				</CardContent>
+				
+				<CardFooter className="flex justify-between">
+					<Button variant="outline">Likes</Button>
+					<Button variant="outline">Comments</Button>
+				</CardFooter>
+			</Card>
         </MainContainer>
     )
 }
