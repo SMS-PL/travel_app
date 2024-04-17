c4052eadb0d902a93e3c94e00ea444823ec9969a
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-04-17 10:23:08 +0200

    ✨ add loading sceleton for posts

--- frontend/travel-app-frontend/src/pages/Home/Home.jsx
+++ frontend/travel-app-frontend/src/pages/Home/Home.jsx
@@ -3,8 +3,18 @@ import useAuthHeader from 'react-auth-kit/hooks/useAuthHeader';
 import MainContainer from "@/components/MainContainer/MainContainer";
 import { useState, useEffect } from 'react';
 
+import { Button } from "@/components/ui/button"
 import Post from "@/components/Post/Post";
 import AddPost from "@/components/AddPost/AddPost";
+
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 
 import {
 	Pagination,
@@ -14,14 +24,17 @@ import {
 	PaginationLink,
 	PaginationNext,
 	PaginationPrevious,
-} from "@/components/ui/pagination"
-  
+} from "@/components/ui/pagination";
+import { Skeleton } from "@/components/ui/skeleton";
+
 const Home = () => {
 	const [posts, setPosts] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 	const authHeader = useAuthHeader();
 	const navigate = useNavigate();
 
     useEffect(() => {
+		setIsLoading(true);
 		fetch("http://localhost:5000/api/v1/posts/feed?feedType=home&pageSize=10&pageNumber=0", {
 			method: 'GET',
 			headers: {
@@ -37,6 +50,7 @@ const Home = () => {
 		})
 		.then(data => {
 			setPosts(data.content);
+			setIsLoading(false);
 		})
 		.catch(error => {
 			console.log(error.message);
@@ -48,18 +62,47 @@ const Home = () => {
 
     return (
         <MainContainer type="homeFeed">
-			{posts.map((post) => (
-				<Post
-					key={post.id}
-					id={post.id}
-					content={post.content}
-					countryId={post.countryId}
-					imageUrl={post.imageUrl}
-					authorId={post.authorId}
-					createdAt={post.createdAt}
-					lastUpdated={post.lastUpdated}
-				/>
-			))}
+
+			{isLoading ? (
+				<Card className="mt-5 w-full">
+					<CardHeader className="flex flex-row">
+						<Skeleton className="h-12 w-12 rounded-full" />
+
+					
+						<div className="px-2 w-fit">
+							<CardTitle><Skeleton className="h-4 w-[250px]" /></CardTitle>
+							<CardDescription className="text-nowrap"><Skeleton className="h-4 w-[250px] mt-2" /></CardDescription>
+						</div>
+
+			
+					</CardHeader>
+
+					<CardContent>
+						<Skeleton className="h-14 w-full mb-5" />
+						<Skeleton className="w-full h-[500px]" />
+					</CardContent>
+					
+					<CardFooter className="flex justify-between">
+						<Button variant="outline">Likes</Button>
+						<Button variant="outline">Comments</Button>
+					</CardFooter>
+				</Card>
+
+			) : (
+				posts.map((post) => (
+					<Post
+						key={post.id}
+						id={post.id}
+						content={post.content}
+						countryId={post.countryId}
+						imageUrl={post.imageUrl}
+						authorId={post.authorId}
+						createdAt={post.createdAt}
+						lastUpdated={post.lastUpdated}
+					/>
+				))
+			)}
+
         </MainContainer>
     )
 }
