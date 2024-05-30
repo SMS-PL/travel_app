4fe00707d8351606426a4f9072db558a29b94a63
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-05-30 20:59:25 +0200

    🔨 Change pins data fetching in a new way

--- frontend/travel-app-frontend/src/components/Pins/Pins.jsx
+++ frontend/travel-app-frontend/src/components/Pins/Pins.jsx
@@ -32,6 +32,7 @@ import {
     AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import ReactTimeAgo from 'react-time-ago';
+import PinDialog from "../ui/PinDialog";
 
 function Pins() {
     const authHeader = useAuthHeader();
@@ -43,6 +44,7 @@ function Pins() {
 	const [refetch, setRefetch] = useState(false);
 	const [isLoading, setIsLoading] = useState(true);
 
+    
     useEffect(() => {
         setIsLoading(true);
 
@@ -52,9 +54,9 @@ function Pins() {
         setIsLoading(false);
     }, [refetch]);
 
-    // póki co pobiera 
+
     const fetchPins = (pageNumber, pageSize) => {
-        fetch(`http://localhost:5000/api/v1/pins/?pageNumber=${pageNumber}&pageSize=${pageSize}`, {
+        fetch(`http://localhost:5000/api/v1/pins/friends?pageNumber=${pageNumber}&pageSize=${pageSize}`, {
 			method: 'GET',
 			headers: {
 				'Content-Type': 'application/json', 
@@ -68,7 +70,7 @@ function Pins() {
 			return response.json();
 		})
 		.then(data => {
-			console.log(data);
+			// console.log(data);
             setPinsData(data);
 		})
 		.catch(error => {
@@ -78,34 +80,35 @@ function Pins() {
 
     const onCreate = async () => {
         if (navigator.geolocation) {
-            try {
-                const position = await new Promise((resolve, reject) => {
-                    navigator.geolocation.getCurrentPosition(resolve, reject);
-                });
-                const xCoordinate = position.coords.latitude;
-                const yCoordinate = position.coords.longitude;
-                
-                const response = await fetch("http://localhost:5000/api/v1/pins/", {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        "Authorization": authHeader,
-                    },
-                    body: JSON.stringify({
-                        "localization": {
-                            "x": xCoordinate,
-                            "y": yCoordinate
-                        }
-                    }),
-                });
-    
+            const position = await new Promise((resolve, reject) => {
+                navigator.geolocation.getCurrentPosition(resolve, reject);
+            });
+            const xCoordinate = position.coords.latitude;
+            const yCoordinate = position.coords.longitude;
+
+            fetch("http://localhost:5000/api/v1/pins/", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json', 
+                    "Authorization": authHeader,
+                },
+                body: JSON.stringify({
+                    "localization": {
+                        "x": xCoordinate,
+                        "y": yCoordinate
+                    }
+                }),
+            })
+            .then(response => {
                 if (!response.ok) {
-                    throw new Error('Błąd sieci!');
+                    return response.json().then(err => {
+                        throw new Error(JSON.stringify(err));
+                    });
                 }
-    
-                const data = await response.json();
+                return response.json();
+            })
+            .then(data => {
                 console.log(data);
-    
                 setRefetch(true);
     
                 toast({
@@ -113,18 +116,23 @@ function Pins() {
                     description: "Successfully add pin!",
                     className: "bg-green-800"
                 });
-            } catch (error) {
-                console.log(error.message);
+            })
+            .catch(error => {
+                console.log(error);
+                const errorMessage = JSON.parse(error.message);
+    
                 toast({
                     variant: "destructive",
                     title: "Uh oh! Something went wrong.",
-                    description: error.message,
-                });
-            }
+                    description: errorMessage.message,
+                })
+            });
+            
         } else {
             console.log("Nie uzyskano lokalizacji!");
         }
     };
+
 
     return (
         <Card className="w-full">
@@ -136,8 +144,8 @@ function Pins() {
 
             <CardContent className="flex flex-row gap-4 justify-center items-start">
                 <div className="flex flex-col justify-center items-center">
-                    <button onClick={onCreate} className="w-[50px] h-[50px] rounded-full bg-secondary flex border-dashed border-2 border-white justify-center items-center">
-                        <Icons.plusEmpty className="h-3 w-3 fill-white" />
+                    <button onClick={onCreate} className="w-[50px] h-[50px] rounded-full bg-secondary flex border-dashed border-2 border-current justify-center items-center">
+                        <Icons.plusEmpty className="h-3 w-3 fill-current" />
                     </button>
                     <p className="mt-1 text-gray-500 text-sm">Check-in</p>
                 </div>
@@ -152,58 +160,17 @@ function Pins() {
                         <Skeleton className="w-[50px] h-[50px] rounded-full"/>
                     </>
 
-                ) : pinsData.content.map(pin => {
+                ) : pinsData.content.map(userPinsArray => {
                     return (
-
-                        <AlertDialog key={`userPin${pin.id}`} className="max-w-full w-[700px]">
-                            <AlertDialogTrigger>
-                                <div  className="flex flex-col justify-center items-center ">
-                                    <div className="w-[50px] h-[50px] rounded-full bg-secondary flex justify-center items-center border-solid border-2 border-primary cursor-pointer">
-                                    </div>
-
-                                    <div className="mt-1 text-gray-500 text-sm flex flex-col text-center">
-                                        <p>{pin.author.firstName}</p>
-                                    </div>
-                                </div>
-                            </AlertDialogTrigger>
-
-                            <AlertDialogContent>
-                                <AlertDialogHeader>
-                                    <AlertDialogTitle className="flex justify-between mb-3">
-                                        <div>
-                                            <p className="text-white font-bold text-md">{pin.author.firstName} {pin.author.lastName}</p>
-                                            {/* <p className="text-sm text-gray-500">{pin.author.createdAt}</p> */}
-                                            <ReactTimeAgo timeStyle="round" date={pin.createdAt} locale="en-US" className="text-sm text-gray-500"/>
-                                        </div>
-                                        <AlertDialogCancel>Close</AlertDialogCancel>
-                                    </AlertDialogTitle>
-
-                                    <AlertDialogDescription className="flex flex-col">
-                                        <div className="w-full rounded-xl border bg-card text-card-foreground shadow">
-                                            <img className="w-full rounded-md" src="https://lh3.googleusercontent.com/ILV2_Xm--SVih4MElRmFeu6k6YIdxD72ic7a3-qWJgGgDcyU_UzIPOTg8vEr4zjH5UhYlmFKpVV2ukBJOz_I_d24vlS96b2dE2873T53=rw-e365-w900" alt="" srcset="" />
-                                        </div>
-                                        <div className="w-full flex flex-col justify-center items-center mt-3">
-                                            <p className="text-lg">{pin.city}, {pin.countryName}</p>
-                                        </div>
-                                        
-                                        {/* This action cannot be undone. This will permanently delete the post from our servers. */}
-                                    </AlertDialogDescription>
-                                </AlertDialogHeader>
-
-                                {/* <AlertDialogFooter>
-                                    <AlertDialogAction>Continue</AlertDialogAction>
-                                </AlertDialogFooter> */}
-
-                            </AlertDialogContent>
-
-                        </AlertDialog>
-
-
+                        <PinDialog
+                            key={`userPin-${userPinsArray[Object.keys(userPinsArray)][0].id}`}
+                            userPinsArray={userPinsArray}
+                            setRefetch={setRefetch}
+                        />
                     );
                 })}
 
             </CardContent>
-            {/* <CardFooter className="flex justify-between"></CardFooter> */}
         </Card>
     )
 }
