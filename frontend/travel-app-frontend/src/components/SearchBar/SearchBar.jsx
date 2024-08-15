1a25041a8680d00f4d10440e5018aa1f36a315b7
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-08-15 12:59:53 +0200

    🔨 add responsiveness of searchbar

--- frontend/travel-app-frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/travel-app-frontend/src/components/SearchBar/SearchBar.jsx
@@ -14,6 +14,11 @@ import { Icons } from "@/components/icons";
 import { Input } from "@/components/ui/input";
 import FriendshipRowView from "@/components/SearchBar/FriendshipRowView";
 import HoverUserInfo from "@/components/ui/HoverUserInfo";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
 
 const SearchBar = ({placeholder, ...props}) => {
     const authHeader = useAuthHeader();
@@ -50,8 +55,59 @@ const SearchBar = ({placeholder, ...props}) => {
     }; 
 
     return (
-        <div className="max-w-full w-[300px]">
-            <div className="relative ml-auto flex-1 md:grow-0" >
+        <div className="max-w-full w-[300px] flex justify-start items-center">
+            <div className="flex flex-row justify-start items-center">
+                {/* DESKOPT */}
+                <div className="relative md:grow-0 hidden md:flex" >
+                    <Icons.searchEmpty className="absolute fill-current left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                    <Input
+                        type="search"
+                        placeholder={placeholder}
+                        className="rounded-lg backdrop-blur pl-8 w-[225px]"
+                        onChange={(e) => {
+                            e.target.value.length == 0 ? setIsOpenSearchBar(false) : setIsOpenSearchBar(true);
+                            fetchUsers(e.target.value);
+                        }}
+                        onBlur={(e) => {
+                            e.target.value = "";
+                            setIsOpenSearchBar(false);
+                        }}
+                    />
+                </div>
+                
+                {/* MOBILE */}
+                <div className="flex md:hidden">
+                    <Popover>
+                        <PopoverTrigger className="flex justify-center items-center">
+                            <Icons.searchEmpty className="fill-current h-5 w-5 text-muted-foreground" />
+                        </PopoverTrigger>
+
+                        <PopoverContent 
+                            className="absolute top-[-30px] left-[15px] p-0 border-0 w-0"
+                            
+                        >
+                            {/* <Icons.searchEmpty className="absolute fill-current left-[20px] top-[26px] z-20 h-4 w-4 text-muted-foreground" /> */}
+
+                            <Input
+                                type="search"
+                                placeholder={placeholder}
+                                className="rounded-lg backdrop-blur block md:hidden w-[215px]"
+                                onChange={(e) => {
+                                    e.target.value.length == 0 ? setIsOpenSearchBar(false) : setIsOpenSearchBar(true);
+                                    fetchUsers(e.target.value);
+                                }}
+                                onBlur={(e) => {
+                                    setIsOpenSearchBar(false);
+                                    e.target.value = "";
+                                }}
+                            />
+                        </PopoverContent>
+                    </Popover>    
+                </div>
+            </div>
+
+
+            {/* <div className="relative ml-auto flex-1 md:grow-0" >
                 <Icons.searchEmpty className="absolute fill-current left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
                 <Input
                     type="search"
@@ -59,12 +115,11 @@ const SearchBar = ({placeholder, ...props}) => {
                     className="rounded-lg backdrop-blur pl-8 w-full "
                     onChange={(e) => {
                         e.target.value.length == 0 ? setIsOpenSearchBar(false) : setIsOpenSearchBar(true);
-                        // setUsersData(e.target.value);
-                        // console.log(e.target.value)
                         fetchUsers(e.target.value);
                     }}
                 />
-            </div>
+
+            </div> */}
 
             {isOpenSearchBar && (
                 <Card className="absolute top-[50px] w-fit" >
