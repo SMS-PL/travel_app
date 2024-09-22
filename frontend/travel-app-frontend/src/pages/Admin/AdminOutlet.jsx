d83203a0018681435206b6c2d4b90266b429220d
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2024-09-22 19:52:19 +0200

    🐛 bug fix

--- frontend/travel-app-frontend/src/pages/Admin/AdminOutlet.jsx
+++ frontend/travel-app-frontend/src/pages/Admin/AdminOutlet.jsx
@@ -2,18 +2,24 @@ import react, {useEffect} from 'react';
 import { Navigate, Outlet } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import useAuthHeader from 'react-auth-kit/hooks/useAuthHeader';
+import useIsAuthenticated from "react-auth-kit/hooks/useIsAuthenticated";
 
 const AdminOutlet = ({ fallbackPath }) => {
+    const isAuthenticated = useIsAuthenticated();
     const token = useAuthHeader();
-    const user = jwtDecode(token);
-
-    // Sprawdzamy, czy użytkownik jest zalogowany i jest udminem
-    if(token && user.role == "ROLE_ADMIN") {
-        console.log("ADMIN!!!!");
-        return <Outlet />;
-    } 
     
-    return <Navigate to={fallbackPath} replace />;
+    // sprawdzamy, czy użytkownik jest zalogowany
+    if(isAuthenticated()) {
+        const user = jwtDecode(token);
+    
+        // Sprawdzamy, czy użytkownik jest adminem
+        if(token && user.role == "ROLE_ADMIN") return <Outlet />;
+        return <Navigate to={fallbackPath} replace />;
+        
+    } else {
+        return <Navigate to={"/login"} replace />;
+    }
+
 };
 
 export default AdminOutlet;
